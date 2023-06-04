2db9cff0b91c5adcfd74f3c6e3c39134a71b28a1
Stefan El Abdellaoui <sth.el-abdellaoui@mci4me.at>
2023-06-04 14:22:22 +0200

    ci(project): Add Dependencies for Project (Vaadin, Springframework, etc.)
    CLOSES: Issue SP-51 and SP-8

--- ErrorAnnihilator/src/main/java/com/application/Application.java
+++ ErrorAnnihilator/src/main/java/com/application/Application.java
@@ -1,8 +1,11 @@
 package com.application;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import org.springframework.boot.SpringApplication;
 
-public class Application {
+public class Application implements AppShellConfigurator {
     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
         System.out.println("Hello world!");
     }
 }
