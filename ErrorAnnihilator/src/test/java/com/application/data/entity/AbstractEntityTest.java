97084f59f82a3a51724a2eeb4647d1f057a9eb57
am5393 <am5393@mci4me.at>
2023-06-08 19:19:16 +0200

    Test GetSetId-Method
    Sucessful testing of the GetSetId-Method from the Abstract Entity

--- ErrorAnnihilator/src/test/java/com/application/data/entity/AbstractEntityTest.java
+++ ErrorAnnihilator/src/test/java/com/application/data/entity/AbstractEntityTest.java
@@ -1,4 +1,25 @@
 package com.application.data.entity;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class AbstractEntityTest {
+
+    private static class ConcreteEntity extends AbstractEntity {
+        // Additional properties and methods for testing purposes
+    }
+
+    @Test
+    public void testGetSetId() {
+        // Arrange
+        AbstractEntity entity = new ConcreteEntity();
+        Long id = 1L;
+
+        // Act
+        entity.setId(id);
+        Long retrievedId = entity.getId();
+
+        // Assert
+        Assertions.assertEquals(id, retrievedId);
+    }
 }
