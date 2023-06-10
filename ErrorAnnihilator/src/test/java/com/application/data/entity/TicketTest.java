d7996d814e73928237ac13256f04c3e48304806c
am5393 <am5393@mci4me.at>
2023-06-10 19:34:24 +0200

    Instance if Ticket Class (Before Each)
    Create an instance of the Ticket class in the setUp method before each test

--- ErrorAnnihilator/src/test/java/com/application/data/entity/TicketTest.java
+++ ErrorAnnihilator/src/test/java/com/application/data/entity/TicketTest.java
@@ -1,2 +1,24 @@
-package com.application.data.entity;public class TicketTest {
+package com.application.data.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TicketTest {
+    private Ticket ticket;
+    private User ticketCreator;
+    private List<User> assignedUsers;
+
+    @BeforeEach
+    public void setUp() {
+        //create an instance of the Ticket class in the setUp method before each test
+        ticketCreator = new User();
+        assignedUsers = new LinkedList<>();
+        assignedUsers.add(new User());
+        assignedUsers.add(new User());
+        ticket = new Ticket("Test Ticket", "Bug", "Description",
+                 new TicketStatus("Open"), ticketCreator, assignedUsers);
+    }
+
 }
