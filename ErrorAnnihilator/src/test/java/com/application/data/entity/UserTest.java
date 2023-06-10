1eb6995562b68b96ac02ec7e00d7b3f2f46e5921
am5393 <am5393@mci4me.at>
2023-06-10 19:54:33 +0200

    Test Default Constructor User
    Sucessful testing of the Default Constructor from the User Entity

--- ErrorAnnihilator/src/test/java/com/application/data/entity/UserTest.java
+++ ErrorAnnihilator/src/test/java/com/application/data/entity/UserTest.java
@@ -1,2 +1,19 @@
-package com.application.data.entity;public class UserTest {
+package com.application.data.entity;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+    @Test
+    public void testDefaultConstructor() {
+        User user = new User();
+        Assertions.assertEquals("", user.getFirstName());
+        Assertions.assertEquals("", user.getLastName());
+        Assertions.assertEquals("", user.getUserName());
+        Assertions.assertEquals("", user.getEmail());
+        Assertions.assertEquals("", user.getDummyPassword());
+        Assertions.assertEquals("", user.getUserRole());
+        Assertions.assertNull(user.getCreatedTickets());
+        Assertions.assertNull(user.getTicket());
+    }
 }
