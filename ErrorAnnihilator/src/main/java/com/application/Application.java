4c0c3c4bd5009caf3b0525b00329cc4dc14610c7
Isabelle <mi6164@mci4me.at>
2023-06-14 10:16:47 +0200

    Hallo! Ich habe jetzt versucht die LoginView zu erstellen. Mit kleinem Header - falls nicht gewünscht, kann ich das noch ändern. Und die Offline - Seite, wobei ich das Bild noch anpassen werde. lG

--- ErrorAnnihilator/src/main/java/com/application/Application.java
+++ ErrorAnnihilator/src/main/java/com/application/Application.java
@@ -1,9 +1,19 @@
 package com.application;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 
-
+@SpringBootConfiguration
+@Theme(value="ErrorAnnihilator")
+@PWA(
+        name="ErrorAnnihilator",
+        shortName = "E.A.",
+        offlinePath = "offline.html",
+        offlineResources = {"images/logo.png", "images/offline.png"}
+)
 public class Application implements AppShellConfigurator {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ ErrorAnnihilator/src/main/java/com/application/view/LoginOverlayHeader.java
@@ -0,0 +1,22 @@
+
+package com.application.view;
+
+        import com.vaadin.flow.component.login.LoginOverlay;
+        import com.vaadin.flow.component.html.Div;
+        import com.vaadin.flow.router.Route;
+
+@Route("login-overlay-header")
+public class LoginOverlayHeader extends Div {
+
+    public LoginOverlayHeader() {
+        LoginOverlay loginOverlay = new LoginOverlay();
+        loginOverlay.setTitle("ErrorAnnihilator");
+        loginOverlay.setDescription("♥ We will get rid of your bugs ♥");
+        add(loginOverlay);
+        loginOverlay.setOpened(true);
+        loginOverlay.getElement().setAttribute("no-autofocus", "");
+    }
+
+}
+
+
