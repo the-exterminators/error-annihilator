46bf5a7339344b5479ea4cdb8105f5193a3de1d2
Eldienkay <kd3012@mci4me.at>
2023-06-14 18:23:52 +0200

    securityconfig
    Securityconfig and Securityservice with in-memory authentification added

---
+++ ErrorAnnihilator/src/main/java/com/application/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.application.security;
+
+import com.application.views.LoginView;
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends VaadinWebSecurity {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/images/*.png").permitAll();
+        super.configure(http);
+        setLoginView(http, LoginView.class);
+    }
+
+
+    // In-memory credentials to be changed later on
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.builder()
+                .username("user")
+                // password = password with this hash, don't tell anybody :-)
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
---
+++ ErrorAnnihilator/src/main/java/com/application/views/LoginView.java
@@ -0,0 +1,45 @@
+package com.application.views;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+//View aus dem Tutorial quasi 1 zu 1 uebernommen! Dient nur zum testen :)
+//@PermitAll muss noch in alle Views eingefügt werden!
+//@Route muessen auch noch definiert werden in den anderen Views
+//Logout Button muss auch noch in der Mainview hinzugefuegt werden
+
+@Route("login") 
+@PageTitle("Login | ErrorAnnihilator")
+@AnonymousAllowed
+public class LoginView extends VerticalLayout implements BeforeEnterObserver {
+
+	private final LoginForm login = new LoginForm(); 
+
+	public LoginView(){
+		addClassName("login-view");
+		setSizeFull(); 
+		setAlignItems(Alignment.CENTER);
+		setJustifyContentMode(JustifyContentMode.CENTER);
+
+		login.setAction("login"); 
+
+		add(new H1("ErrorAnnihilator"), login);
+	}
+
+	@Override
+	public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
+		// inform the user about an authentication error
+		if(beforeEnterEvent.getLocation()  
+        .getQueryParameters()
+        .getParameters()
+        .containsKey("error")) {
+            login.setError(true);
+        }
+	}
+}
