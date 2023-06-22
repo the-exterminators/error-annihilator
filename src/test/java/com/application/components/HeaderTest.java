8ef976606a740f40088dce8cb7773168c097a6f3
am5393 <am5393@mci4me.at>
2023-06-22 13:25:00 +0200

    Update HeaderTest.java
    Testing if all elements in the header are properly shown

--- src/test/java/com/application/components/HeaderTest.java
+++ src/test/java/com/application/components/HeaderTest.java
@@ -14,8 +14,10 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HeaderTest {
     private WebDriver driver;
@@ -44,9 +46,41 @@ public class HeaderTest {
 
         // Locate the image element using the src attribute value
         WebElement imageElement = driver.findElement(By.cssSelector("img[src='images/logo_big_mixed.svg']"));
-
         assertTrue(imageElement.isDisplayed());
         // Add more assertions as needed
+    }
+
+    @Test
+    public void testCreateDrawer() {
+        driver.get("http://localhost:8080"); // Replace with your actual URL
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Adjust the timeout as needed
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("input-vaadin-text-field-12")));
+
+        // Expand the toggle
+        WebElement toggleButton = driver.findElement(By.xpath("//vaadin-drawer-toggle[@aria-expanded='false']")); // Replace "toggle-button" with the actual class name of the toggle button
+        toggleButton.click();
+
+        // Wait for the toggle to expand and the ticket search input to become visible
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input-vaadin-text-field-12")));
+
+        // Locate the ticket search input field
+        WebElement ticketSearchInput = driver.findElement(By.id("input-vaadin-text-field-12"));
+        assertTrue(ticketSearchInput.isDisplayed());
+        // Add more assertions or interactions with the ticket search input as needed
+
+        // Locate the search button
+        WebElement searchButton = driver.findElement(By.cssSelector("vaadin-icon[slot='prefix'][icon='vaadin:search']"));
+        assertTrue(searchButton.isDisplayed());
+        // Add more assertions or interactions with the search button as needed
+
+        // Locate the router links
+        List<WebElement> routerLinks = driver.findElements(By.tagName("a"));
+        assertEquals(3, routerLinks.size());
+
+        // Locate the logout button
+        WebElement logoutButton = driver.findElement(By.cssSelector("vaadin-button[tabindex='0'][role='button']"));
+        assertTrue(logoutButton.isDisplayed());
     }
 }
 
